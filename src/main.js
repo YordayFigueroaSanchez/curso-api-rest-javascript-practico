7018d43d9ab54e49e2f69d6f6a6ae820baffc3ec
yfsanchez <yfsanchez@gmail.com>
2023-03-06 01:30:57 -0300

    consultando la api
    consultando la api

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 
-console.log('API URL = https://luhiulhiusf.com/ahiuhiudaiu?api_jey=' + API_KEY)
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+    console.log({data});
+}
+
+getTrendingMoviesPreview();
