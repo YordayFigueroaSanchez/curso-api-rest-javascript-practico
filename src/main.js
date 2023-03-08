ed8b2bca4a618824defb369488494244c5e56de2
yfsanchez <yfsanchez@gmail.com>
2023-03-07 21:39:53 -0300

    class 05 Lista de películas en tendencia

--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@ async function getTrendingMoviesPreview() {
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await res.json();
     console.log({data});
+
+    const movies = data.results;
+    movies.forEach(movie => {
+        const trendingMoviesConteiner = document.querySelector('#trendingPreview .trendingPreview-movieList');
+
+        const movieConteiner = document.createElement('div');
+        movieConteiner.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', "https://image.tmdb.org/t/p/w300" + movie.poster_path);
+
+        movieConteiner.appendChild(movieImg);
+
+        trendingMoviesConteiner.appendChild(movieConteiner);
+    });
 }
 
 getTrendingMoviesPreview();
